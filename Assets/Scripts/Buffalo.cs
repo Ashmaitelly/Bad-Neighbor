96a0af35f852b8f09ff895a1a9ab26ffb4bb72de
Ashmaitelly <ashmaitelly@gmail.com>
2024-01-28 16:25:46 +0200

    Added man hitting himself

--- Assets/Scripts/Buffalo.cs
+++ Assets/Scripts/Buffalo.cs
@@ -6,8 +6,7 @@ public class Bufallo : MonoBehaviour
 {
     public float moveSpeed = 0.5f; // Adjust the speed as needed
      private Animator anim;
-    Vector3 moveDirection = new Vector3(horizontalInput, 0, 0);
-    transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    
     private Vector3 originalScale; 
     // Start is called before the first frame update
     void Start()
